7cae812bfb5a56945419cb3bc858b8d2c803b4a8
Python for Geopeople user <geo@pb-john-the-sad.novalocal>
2016-09-16 07:21:50 +0000

    hellou

---
+++ Riikka1.py
@@ -0,0 +1,10 @@
+In [1]: months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+In [2]: daysInMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+In [3]: SelectedMonth = 8
+
+In [4]: 
+
+
+
